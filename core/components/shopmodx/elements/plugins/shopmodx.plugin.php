b27613d81d41294a867ae9292ba54749b6741b78
Sergey <proxyfabio@users.noreply.github.com>
2013-12-24 12:19:03 -0800

    Fix tree bug
    Fix errors if tree doesn't exists.

--- core/components/shopmodx/elements/plugins/shopmodx.plugin.php
+++ core/components/shopmodx/elements/plugins/shopmodx.plugin.php
@@ -28,45 +28,47 @@ Ext.onReady(function(){
     // Получаем дерево
     var tree = Ext.getCmp('modx-resource-tree');
     // Описываем правила разрешенных дочерних ресурсов
-    var resourcesRules = {$resourcesRulesJSON};
-    // Прописываем функцию копирования значений объекта,
-    // так как у нас проблемы на уровне ссылок на объекты,
-    // то есть изменяя одну переменную, изменяется и другая, если объект общий
-    var copyObject = function(from){
-        var to = {};
-        for(i in from){
-            to[i] = from[i];
-        }
-        return to;
-    }    
-    // Фиксируем изначальный набор классов
-    var classes = copyObject(MODx.config.resource_classes);
-
-    // Навешиваем функцию изменения набора классов
-    // при создании контекстного меню
-    tree.on('loadCreateMenus', function(types){
-        var node = this.getSelectionModel().getSelectedNode()
-            ,classKey;
-
-        if(resourcesRules && node && node.attributes 
-                && (classKey = node.attributes.classKey)
-                && resourcesRules[classKey]
-        ){
-            for(var i in types){
-                if(!resourcesRules[classKey].in_array(i)){
-                    delete  types[i];
-                }
+    if(typeof tree !== 'undefined'){
+        var resourcesRules = {$resourcesRulesJSON};
+        // Прописываем функцию копирования значений объекта,
+        // так как у нас проблемы на уровне ссылок на объекты,
+        // то есть изменяя одну переменную, изменяется и другая, если объект общий
+        var copyObject = function(from){
+            var to = {};
+            for(i in from){
+                to[i] = from[i];
             }
+            return to;
         }    
-        return true;
-    }, tree);
+        // Фиксируем изначальный набор классов
+        var classes = copyObject(MODx.config.resource_classes);
     
-    // Навешиваем функцию восстановления набора классов
-    // Эта функция выполнится после формирования меню, так как навешено позже
-    // базового метода дерева ресурсов
-    tree.on('contextmenu', function(){
-        MODx.config.resource_classes = copyObject(classes);
-    });
+        // Навешиваем функцию изменения набора классов
+        // при создании контекстного меню
+        tree.on('loadCreateMenus', function(types){
+            var node = this.getSelectionModel().getSelectedNode()
+                ,classKey;
+    
+            if(resourcesRules && node && node.attributes 
+                    && (classKey = node.attributes.classKey)
+                    && resourcesRules[classKey]
+            ){
+                for(var i in types){
+                    if(!resourcesRules[classKey].in_array(i)){
+                        delete  types[i];
+                    }
+                }
+            }    
+            return true;
+        }, tree);
+        
+        // Навешиваем функцию восстановления набора классов
+        // Эта функция выполнится после формирования меню, так как навешено позже
+        // базового метода дерева ресурсов
+        tree.on('contextmenu', function(){
+            MODx.config.resource_classes = copyObject(classes);
+        });
+    }
 }); 
 </script>
 JS;
